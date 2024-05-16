AlanLuu/lox-go

commit 5d4df4bc43e196c1e46f5db8197eb14cb13f69b3

Update string representations of list and string methods

ast/loxlist.go
@@ -20,6 +20,8 @@ func ListIndexOutOfRange(index int64) string {
 	return fmt.Sprintf("List index %v out of range.", index)
 }
 
+var listFuncs = make(map[string]*struct{ ProtoLoxCallable })
+
 type LoxList struct {
 	elements list.List[Expr]
 }
@@ -38,11 +40,20 @@ func (l *LoxList) Equals(obj any) bool {
 }
 
 func (l *LoxList) Get(name token.Token) (any, error) {
-	listFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (struct{ ProtoLoxCallable }, error) {
-		s := struct{ ProtoLoxCallable }{}
+	methodName := name.Lexeme
+	if method, ok := listFuncs[methodName]; ok {
+		return method, nil
+	}
+	listFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (*struct{ ProtoLoxCallable }, error) {
+		s := &struct{ ProtoLoxCallable }{}
 		s.arityMethod = func() int { return arity }
 		s.callMethod = method
-		s.stringMethod = func() string { return "<native list fn>" }
+		s.stringMethod = func() string {
+			return fmt.Sprintf("<native list fn %v at %p>", methodName, s)
+		}
+		if _, ok := listFuncs[methodName]; !ok {
+			listFuncs[methodName] = s
+		}
 		return s, nil
 	}
 	indexOf := func(obj any) int64 {
@@ -87,7 +98,6 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 		}
 		return argList
 	}
-	methodName := name.Lexeme
 	argMustBeType := func(theType string) (any, error) {
 		errStr := fmt.Sprintf("Argument to 'list.%v' must be a %v.", methodName, theType)
 		return nil, loxerror.RuntimeError(name, errStr)

ast/loxstring.go
@@ -12,6 +12,8 @@ import (
 	"github.com/AlanLuu/lox/util"
 )
 
+var strFuncs = make(map[string]*struct{ ProtoLoxCallable })
+
 type LoxString struct {
 	str   string
 	quote byte
@@ -49,11 +51,20 @@ func (l *LoxString) Equals(obj any) bool {
 }
 
 func (l *LoxString) Get(name token.Token) (any, error) {
-	strFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (struct{ ProtoLoxCallable }, error) {
-		s := struct{ ProtoLoxCallable }{}
+	methodName := name.Lexeme
+	if method, ok := strFuncs[methodName]; ok {
+		return method, nil
+	}
+	strFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (*struct{ ProtoLoxCallable }, error) {
+		s := &struct{ ProtoLoxCallable }{}
 		s.arityMethod = func() int { return arity }
 		s.callMethod = method
-		s.stringMethod = func() string { return "<native string fn>" }
+		s.stringMethod = func() string {
+			return fmt.Sprintf("<native string fn %v at %p>", methodName, s)
+		}
+		if _, ok := strFuncs[methodName]; !ok {
+			strFuncs[methodName] = s
+		}
 		return s, nil
 	}
 	padString := func(initialStr string, finalStrLen int64, arg any, padAtStart bool) (string, bool) {
@@ -88,7 +99,6 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 
 		return builder.String(), useDoubleQuote
 	}
-	methodName := name.Lexeme
 	argMustBeType := func(theType string) (any, error) {
 		errStr := fmt.Sprintf("Argument to 'string.%v' must be a %v.", methodName, theType)
 		return nil, loxerror.RuntimeError(name, errStr)

ast/mathfuncs.go
@@ -21,11 +21,11 @@ func (i *Interpreter) defineMathFuncs() {
 		false,
 	}
 	mathFunc := func(name string, arity int, method func(*Interpreter, list.List[any]) (any, error)) {
-		s := struct{ ProtoLoxCallable }{}
+		s := &struct{ ProtoLoxCallable }{}
 		s.arityMethod = func() int { return arity }
 		s.callMethod = method
 		s.stringMethod = func() string {
-			return fmt.Sprintf("<native fn %v at %p>", name, &s)
+			return fmt.Sprintf("<native Math fn %v at %p>", name, &s)
 		}
 		mathClass.classProperties[name] = s
 	}

commit 4a5cbf72fa6dbb0c22f833ed687eec2af24c8b4a

Fix major bug with storing list methods Bug introduced in commit 5d4df4bc43e196c1e46f5db8197eb14cb13f69b3

ast/interpreter.go
@@ -295,7 +295,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 						newList.Add(element)
 					}
 				}
-				return &LoxList{newList}, nil
+				return NewLoxList(newList), nil
 			}
 		}
 		return math.NaN(), nil
@@ -541,7 +541,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 				for _, element := range right.elements {
 					newList.Add(element)
 				}
-				return &LoxList{newList}, nil
+				return NewLoxList(newList), nil
 			case nil:
 				return &LoxString{left.String() + "nil", '\''}, nil
 			}
@@ -556,7 +556,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 						newList.Add(element)
 					}
 				}
-				return &LoxList{newList}, nil
+				return NewLoxList(newList), nil
 			}
 			switch right := right.(type) {
 			case int64:
@@ -968,7 +968,7 @@ func (i *Interpreter) visitIndexExpr(expr Index) (any, error) {
 			for i := indexValInt; i < indexEndValInt; i++ {
 				listSlice.Add(indexElement.elements[i])
 			}
-			return &LoxList{listSlice}, nil
+			return NewLoxList(listSlice), nil
 		} else {
 			if indexValInt < 0 || indexValInt >= int64(len(indexElement.elements)) {
 				return nil, loxerror.RuntimeError(expr.Bracket, ListIndexOutOfRange(indexValInt))
@@ -987,7 +987,7 @@ func (i *Interpreter) visitListExpr(expr List) (any, error) {
 		}
 		expr.Elements[index] = evalResult
 	}
-	return &LoxList{expr.Elements}, nil
+	return NewLoxList(expr.Elements), nil
 }
 
 func (i *Interpreter) visitLiteralExpr(expr Literal) (any, error) {

ast/loxlist.go
@@ -20,14 +20,20 @@ func ListIndexOutOfRange(index int64) string {
 	return fmt.Sprintf("List index %v out of range.", index)
 }
 
-var listFuncs = make(map[string]*struct{ ProtoLoxCallable })
-
 type LoxList struct {
 	elements list.List[Expr]
+	methods  map[string]*struct{ ProtoLoxCallable }
+}
+
+func NewLoxList(elements list.List[Expr]) *LoxList {
+	return &LoxList{
+		elements: elements,
+		methods:  make(map[string]*struct{ ProtoLoxCallable }),
+	}
 }
 
 func EmptyLoxList() *LoxList {
-	return &LoxList{list.NewList[Expr]()}
+	return NewLoxList(list.NewList[Expr]())
 }
 
 func (l *LoxList) Equals(obj any) bool {
@@ -41,7 +47,7 @@ func (l *LoxList) Equals(obj any) bool {
 
 func (l *LoxList) Get(name token.Token) (any, error) {
 	methodName := name.Lexeme
-	if method, ok := listFuncs[methodName]; ok {
+	if method, ok := l.methods[methodName]; ok {
 		return method, nil
 	}
 	listFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (*struct{ ProtoLoxCallable }, error) {
@@ -51,8 +57,8 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 		s.stringMethod = func() string {
 			return fmt.Sprintf("<native list fn %v at %p>", methodName, s)
 		}
-		if _, ok := listFuncs[methodName]; !ok {
-			listFuncs[methodName] = s
+		if _, ok := l.methods[methodName]; !ok {
+			l.methods[methodName] = s
 		}
 		return s, nil
 	}
@@ -213,7 +219,7 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 						newList.Add(element)
 					}
 				}
-				return &LoxList{newList}, nil
+				return NewLoxList(newList), nil
 			}
 			return argMustBeType("function")
 		})
@@ -278,7 +284,7 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 				}
 			}
 			flatten(l.elements)
-			return &LoxList{newList}, nil
+			return NewLoxList(newList), nil
 		})
 	case "forEach":
 		return listFunc(1, func(i *Interpreter, args list.List[any]) (any, error) {
@@ -359,7 +365,7 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 						newList.Add(result)
 					}
 				}
-				return &LoxList{newList}, nil
+				return NewLoxList(newList), nil
 			}
 			return argMustBeType("function")
 		})

ast/loxstring.go
@@ -187,7 +187,7 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 				for _, str := range splitSlice {
 					loxList.Add(&LoxString{str, '\''})
 				}
-				return &LoxList{loxList}, nil
+				return NewLoxList(loxList), nil
 			}
 			return argMustBeType("string")
 		})

ast/nativefuncs.go
@@ -51,7 +51,7 @@ func (i *Interpreter) defineNativeFuncs() {
 			for index := int64(0); index < size; index++ {
 				lst.Add(nil)
 			}
-			return &LoxList{lst}, nil
+			return NewLoxList(lst), nil
 		}
 		return nil, loxerror.Error("Argument to 'List' must be a whole number.")
 	})