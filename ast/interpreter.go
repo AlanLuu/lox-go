AlanLuu/lox-go

commit 5d4df4bc43e196c1e46f5db8197eb14cb13f69b3

Update string representations of list and string methods

ast/loxlist.go
@@ -20,6 +20,8 @@ func ListIndexOutOfRange(index int64) string {
 	return fmt.Sprintf("List index %v out of range.", index)
 }
 
+var listFuncs = make(map[string]*struct{ ProtoLoxCallable })
+
 type LoxList struct {
 	elements list.List[Expr]
 }
@@ -38,11 +40,20 @@ func (l *LoxList) Equals(obj any) bool {
 }
 
 func (l *LoxList) Get(name token.Token) (any, error) {
-	listFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (struct{ ProtoLoxCallable }, error) {
-		s := struct{ ProtoLoxCallable }{}
+	methodName := name.Lexeme
+	if method, ok := listFuncs[methodName]; ok {
+		return method, nil
+	}
+	listFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (*struct{ ProtoLoxCallable }, error) {
+		s := &struct{ ProtoLoxCallable }{}
 		s.arityMethod = func() int { return arity }
 		s.callMethod = method
-		s.stringMethod = func() string { return "<native list fn>" }
+		s.stringMethod = func() string {
+			return fmt.Sprintf("<native list fn %v at %p>", methodName, s)
+		}
+		if _, ok := listFuncs[methodName]; !ok {
+			listFuncs[methodName] = s
+		}
 		return s, nil
 	}
 	indexOf := func(obj any) int64 {
@@ -87,7 +98,6 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 		}
 		return argList
 	}
-	methodName := name.Lexeme
 	argMustBeType := func(theType string) (any, error) {
 		errStr := fmt.Sprintf("Argument to 'list.%v' must be a %v.", methodName, theType)
 		return nil, loxerror.RuntimeError(name, errStr)

ast/loxstring.go
@@ -12,6 +12,8 @@ import (
 	"github.com/AlanLuu/lox/util"
 )
 
+var strFuncs = make(map[string]*struct{ ProtoLoxCallable })
+
 type LoxString struct {
 	str   string
 	quote byte
@@ -49,11 +51,20 @@ func (l *LoxString) Equals(obj any) bool {
 }
 
 func (l *LoxString) Get(name token.Token) (any, error) {
-	strFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (struct{ ProtoLoxCallable }, error) {
-		s := struct{ ProtoLoxCallable }{}
+	methodName := name.Lexeme
+	if method, ok := strFuncs[methodName]; ok {
+		return method, nil
+	}
+	strFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (*struct{ ProtoLoxCallable }, error) {
+		s := &struct{ ProtoLoxCallable }{}
 		s.arityMethod = func() int { return arity }
 		s.callMethod = method
-		s.stringMethod = func() string { return "<native string fn>" }
+		s.stringMethod = func() string {
+			return fmt.Sprintf("<native string fn %v at %p>", methodName, s)
+		}
+		if _, ok := strFuncs[methodName]; !ok {
+			strFuncs[methodName] = s
+		}
 		return s, nil
 	}
 	padString := func(initialStr string, finalStrLen int64, arg any, padAtStart bool) (string, bool) {
@@ -88,7 +99,6 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 
 		return builder.String(), useDoubleQuote
 	}
-	methodName := name.Lexeme
 	argMustBeType := func(theType string) (any, error) {
 		errStr := fmt.Sprintf("Argument to 'string.%v' must be a %v.", methodName, theType)
 		return nil, loxerror.RuntimeError(name, errStr)

ast/mathfuncs.go
@@ -21,11 +21,11 @@ func (i *Interpreter) defineMathFuncs() {
 		false,
 	}
 	mathFunc := func(name string, arity int, method func(*Interpreter, list.List[any]) (any, error)) {
-		s := struct{ ProtoLoxCallable }{}
+		s := &struct{ ProtoLoxCallable }{}
 		s.arityMethod = func() int { return arity }
 		s.callMethod = method
 		s.stringMethod = func() string {
-			return fmt.Sprintf("<native fn %v at %p>", name, &s)
+			return fmt.Sprintf("<native Math fn %v at %p>", name, &s)
 		}
 		mathClass.classProperties[name] = s
 	}

commit f79fb1c1e401d954c43012712a9d5cf4ff790559

Fix major bug with storing string methods Bug introduced in commit 5d4df4bc43e196c1e46f5db8197eb14cb13f69b3

ast/interpreter.go
@@ -283,7 +283,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 	handleNumList := func(left float64, right *LoxList) (any, error) {
 		switch expr.Operator.TokenType {
 		case token.PLUS:
-			return handleNumString(left, &LoxString{right.String(), '\''})
+			return handleNumString(left, NewLoxString(right.String(), '\''))
 		case token.STAR:
 			if left <= 0 || len(right.elements) == 0 {
 				return EmptyLoxList(), nil
@@ -408,7 +408,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 		case *LoxString:
 		case *LoxList:
 		default:
-			left = &LoxString{leftAsStringer.String(), '\''}
+			left = NewLoxString(leftAsStringer.String(), '\'')
 		}
 	}
 	if rightAsStringer, ok := right.(fmt.Stringer); ok {
@@ -416,7 +416,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 		case *LoxString:
 		case *LoxList:
 		default:
-			right = &LoxString{rightAsStringer.String(), '\''}
+			right = NewLoxString(rightAsStringer.String(), '\'')
 		}
 	}
 	switch left := left.(type) {
@@ -468,7 +468,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 		case *LoxList:
 			switch expr.Operator.TokenType {
 			case token.PLUS:
-				return &LoxString{strconv.FormatBool(left) + right.String(), '\''}, nil
+				return NewLoxString(strconv.FormatBool(left)+right.String(), '\''), nil
 			case token.STAR:
 				return handleNumList(boolMap[left], right)
 			}
@@ -492,9 +492,9 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 				return left.NewLoxString(left.str + strconv.FormatBool(right)), nil
 			case *LoxString:
 				if left.quote == '"' || right.quote == '"' {
-					return &LoxString{left.str + right.str, '"'}, nil
+					return NewLoxString(left.str+right.str, '"'), nil
 				}
-				return &LoxString{left.str + right.str, '\''}, nil
+				return NewLoxString(left.str+right.str, '\''), nil
 			case *LoxList:
 				return left.NewLoxString(left.str + right.String()), nil
 			case nil:
@@ -521,16 +521,16 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 		case token.PLUS:
 			switch right := right.(type) {
 			case int64:
-				return &LoxString{left.String() + strconv.FormatFloat(float64(right), 'f', -1, 64), '\''}, nil
+				return NewLoxString(left.String()+strconv.FormatFloat(float64(right), 'f', -1, 64), '\''), nil
 			case float64:
 				if math.IsInf(right, 1) {
-					return &LoxString{left.String() + "Infinity", '\''}, nil
+					return NewLoxString(left.String()+"Infinity", '\''), nil
 				} else if math.IsInf(right, -1) {
-					return &LoxString{left.String() + "-Infinity", '\''}, nil
+					return NewLoxString(left.String()+"-Infinity", '\''), nil
 				}
-				return &LoxString{left.String() + strconv.FormatFloat(right, 'f', -1, 64), '\''}, nil
+				return NewLoxString(left.String()+strconv.FormatFloat(right, 'f', -1, 64), '\''), nil
 			case bool:
-				return &LoxString{left.String() + strconv.FormatBool(right), '\''}, nil
+				return NewLoxString(left.String()+strconv.FormatBool(right), '\''), nil
 			case *LoxString:
 				return right.NewLoxString(left.String() + right.str), nil
 			case *LoxList:
@@ -543,7 +543,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 				}
 				return NewLoxList(newList), nil
 			case nil:
-				return &LoxString{left.String() + "nil", '\''}, nil
+				return NewLoxString(left.String()+"nil", '\''), nil
 			}
 		case token.STAR:
 			repeat := func(left *LoxList, right int64) (*LoxList, error) {
@@ -585,7 +585,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 		case *LoxList:
 			switch expr.Operator.TokenType {
 			case token.PLUS:
-				return &LoxString{"nil" + right.String(), '\''}, nil
+				return NewLoxString("nil"+right.String(), '\''), nil
 			case token.STAR:
 				return EmptyLoxList(), nil
 			}
@@ -934,16 +934,16 @@ func (i *Interpreter) visitIndexExpr(expr Index) (any, error) {
 			if indexValInt > indexEndValInt {
 				return EmptyLoxString(), nil
 			}
-			return &LoxString{indexElement.str[indexValInt:indexEndValInt], '\''}, nil
+			return NewLoxString(indexElement.str[indexValInt:indexEndValInt], '\''), nil
 		} else {
 			if indexValInt < 0 || indexValInt >= int64(len(indexElement.str)) {
 				return nil, loxerror.RuntimeError(expr.Bracket, StringIndexOutOfRange(indexValInt))
 			}
 			str := string(indexElement.str[indexValInt])
 			if str == "'" {
-				return &LoxString{str, '"'}, nil
+				return NewLoxString(str, '"'), nil
 			}
-			return &LoxString{str, '\''}, nil
+			return NewLoxString(str, '\''), nil
 		}
 	case *LoxList:
 		if _, ok := indexVal.(int64); !ok {
@@ -1109,7 +1109,7 @@ func (i *Interpreter) visitSetListExpr(expr SetList) (any, error) {
 }
 
 func (i *Interpreter) visitStringExpr(expr String) (any, error) {
-	return &LoxString{expr.Str, expr.Quote}, nil
+	return NewLoxString(expr.Str, expr.Quote), nil
 }
 
 func (i *Interpreter) visitSuperExpr(expr Super) (any, error) {

ast/loxlist.go
@@ -341,7 +341,7 @@ func (l *LoxList) Get(name token.Token) (any, error) {
 						builder.WriteString(loxStr.str)
 					}
 				}
-				return &LoxString{builder.String(), quote}, nil
+				return NewLoxString(builder.String(), quote), nil
 			}
 			return argMustBeType("string")
 		})

ast/loxstring.go
@@ -12,18 +12,22 @@ import (
 	"github.com/AlanLuu/lox/util"
 )
 
-var strFuncs = make(map[string]*struct{ ProtoLoxCallable })
-
 type LoxString struct {
-	str   string
-	quote byte
+	str     string
+	quote   byte
+	methods map[string]*struct{ ProtoLoxCallable }
+}
+
+func NewLoxString(str string, quote byte) *LoxString {
+	return &LoxString{
+		str:     str,
+		quote:   quote,
+		methods: make(map[string]*struct{ ProtoLoxCallable }),
+	}
 }
 
 func EmptyLoxString() *LoxString {
-	return &LoxString{
-		str:   "",
-		quote: '\'',
-	}
+	return NewLoxString("", '\'')
 }
 
 func StringIndexMustBeWholeNum(index any) string {
@@ -52,7 +56,7 @@ func (l *LoxString) Equals(obj any) bool {
 
 func (l *LoxString) Get(name token.Token) (any, error) {
 	methodName := name.Lexeme
-	if method, ok := strFuncs[methodName]; ok {
+	if method, ok := l.methods[methodName]; ok {
 		return method, nil
 	}
 	strFunc := func(arity int, method func(*Interpreter, list.List[any]) (any, error)) (*struct{ ProtoLoxCallable }, error) {
@@ -62,8 +66,8 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 		s.stringMethod = func() string {
 			return fmt.Sprintf("<native string fn %v at %p>", methodName, s)
 		}
-		if _, ok := strFuncs[methodName]; !ok {
-			strFuncs[methodName] = s
+		if _, ok := l.methods[methodName]; !ok {
+			l.methods[methodName] = s
 		}
 		return s, nil
 	}
@@ -141,16 +145,16 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 		})
 	case "lower":
 		return strFunc(0, func(_ *Interpreter, _ list.List[any]) (any, error) {
-			return &LoxString{strings.ToLower(l.str), l.quote}, nil
+			return NewLoxString(strings.ToLower(l.str), l.quote), nil
 		})
 	case "padEnd":
 		return strFunc(2, func(_ *Interpreter, args list.List[any]) (any, error) {
 			if finalStrLen, ok := args[0].(int64); ok {
 				paddedStr, useDoubleQuote := padString(l.str, finalStrLen, args[1], false)
 				if useDoubleQuote {
-					return &LoxString{paddedStr, '"'}, nil
+					return NewLoxString(paddedStr, '"'), nil
 				}
-				return &LoxString{paddedStr, l.quote}, nil
+				return NewLoxString(paddedStr, l.quote), nil
 			}
 			return nil, loxerror.RuntimeError(name, "First argument to 'string.padEnd' must be a whole number.")
 		})
@@ -159,9 +163,9 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 			if finalStrLen, ok := args[0].(int64); ok {
 				paddedStr, useDoubleQuote := padString(l.str, finalStrLen, args[1], true)
 				if useDoubleQuote {
-					return &LoxString{paddedStr, '"'}, nil
+					return NewLoxString(paddedStr, '"'), nil
 				}
-				return &LoxString{paddedStr, l.quote}, nil
+				return NewLoxString(paddedStr, l.quote), nil
 			}
 			return nil, loxerror.RuntimeError(name, "First argument to 'string.padStart' must be a whole number.")
 		})
@@ -171,9 +175,9 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 				if secondStr, secondStrOk := args[1].(*LoxString); secondStrOk {
 					newStr := strings.ReplaceAll(l.str, firstStr.str, secondStr.str)
 					if strings.Contains(newStr, "'") {
-						return &LoxString{newStr, '"'}, nil
+						return NewLoxString(newStr, '"'), nil
 					}
-					return &LoxString{newStr, '\''}, nil
+					return NewLoxString(newStr, '\''), nil
 				}
 				return nil, loxerror.RuntimeError(name, "Second argument to 'string.replace' must be a string.")
 			}
@@ -185,7 +189,7 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 				splitSlice := strings.Split(l.str, loxStr.str)
 				loxList := list.NewList[Expr]()
 				for _, str := range splitSlice {
-					loxList.Add(&LoxString{str, '\''})
+					loxList.Add(NewLoxString(str, '\''))
 				}
 				return NewLoxList(loxList), nil
 			}
@@ -203,10 +207,10 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 			argsLen := len(args)
 			switch argsLen {
 			case 0:
-				return &LoxString{strings.TrimSpace(l.str), l.quote}, nil
+				return NewLoxString(strings.TrimSpace(l.str), l.quote), nil
 			case 1:
 				if loxStr, ok := args[0].(*LoxString); ok {
-					return &LoxString{strings.Trim(l.str, loxStr.str), l.quote}, nil
+					return NewLoxString(strings.Trim(l.str, loxStr.str), l.quote), nil
 				}
 				return argMustBeType("string")
 			}
@@ -274,7 +278,7 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 		})
 	case "upper":
 		return strFunc(0, func(_ *Interpreter, _ list.List[any]) (any, error) {
-			return &LoxString{strings.ToUpper(l.str), l.quote}, nil
+			return NewLoxString(strings.ToUpper(l.str), l.quote), nil
 		})
 	case "zfill":
 		return strFunc(1, func(_ *Interpreter, args list.List[any]) (any, error) {
@@ -302,7 +306,7 @@ func (l *LoxString) Get(name token.Token) (any, error) {
 				} else {
 					finalStr = paddedStr
 				}
-				return &LoxString{finalStr, quote}, nil
+				return NewLoxString(finalStr, quote), nil
 			}
 			return argMustBeType("whole number")
 		})

ast/nativefuncs.go
@@ -27,9 +27,9 @@ func (i *Interpreter) defineNativeFuncs() {
 			codePoint := rune(codePointNum)
 			character := string(codePoint)
 			if codePoint == '\'' {
-				return &LoxString{character, '"'}, nil
+				return NewLoxString(character, '"'), nil
 			}
-			return &LoxString{character, '\''}, nil
+			return NewLoxString(character, '\''), nil
 		}
 		return nil, loxerror.Error("Argument to 'chr' must be a whole number.")
 	})