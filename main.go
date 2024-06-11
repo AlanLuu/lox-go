5fac19539dcb4095223b05e3560272c9d00e8bcb
Alan Luu <alanluu4@gmail.com>
2024-06-11 10:02:18 -0700

    Fix newline counting when reading in piped program

--- main.go
+++ main.go
@@ -72,63 +72,61 @@ func interactiveMode() int {
 	defer l.Close()
 
 	interpreter := ast.NewInterpreter()
-	stdinFromTerminal := util.StdinFromTerminal()
-	numSpacesIndent := 2
-
-outer:
-	for {
-		var program strings.Builder
-		scopeLevel := 0
+	if util.StdinFromTerminal() {
+		numSpacesIndent := 2
+	outer:
 		for {
-			if stdinFromTerminal {
+			var program strings.Builder
+			scopeLevel := 0
+			for {
 				if scopeLevel > 0 {
 					indent := strings.Repeat(" ", numSpacesIndent*scopeLevel)
 					l.SetPrompt(NEXT_LINE_PROMPT + indent)
 				} else {
 					l.SetPrompt(PROMPT)
 				}
-			}
 
-			userInput, readError := l.Readline()
-			if readError == readline.ErrInterrupt {
-				continue
-			} else if readError == io.EOF {
-				if stdinFromTerminal {
+				userInput, readError := l.Readline()
+				if readError == readline.ErrInterrupt {
+					continue
+				} else if readError == io.EOF {
 					break outer
-				} else {
-					break
 				}
-			}
 
-			if len(userInput) == 0 {
-				if !stdinFromTerminal || scopeLevel > 0 {
-					program.WriteByte('\n')
+				if len(userInput) == 0 {
+					if scopeLevel > 0 {
+						program.WriteByte('\n')
+					}
+					continue
 				}
-				continue
-			}
-			userInput = strings.TrimSpace(userInput)
-			program.WriteString(userInput)
-			if stdinFromTerminal {
+				userInput = strings.TrimSpace(userInput)
+				program.WriteString(userInput)
 				leftBraceCount, rightBraceCount := util.CountBraces(userInput)
 				scopeLevel += (leftBraceCount - rightBraceCount)
 				if scopeLevel <= 0 {
 					break
 				}
+				program.WriteByte('\n')
 			}
-			program.WriteByte('\n')
+
+			sc := scanner.NewScanner(program.String())
+			resultError := run(sc, interpreter)
+			if resultError != nil {
+				loxerror.PrintErrorObject(resultError)
+			}
+		}
+	} else {
+		program, readErr := io.ReadAll(os.Stdin)
+		if readErr != nil {
+			loxerror.PrintErrorObject(readErr)
+			return 1
 		}
 
-		sc := scanner.NewScanner(program.String())
+		sc := scanner.NewScanner(string(program))
 		resultError := run(sc, interpreter)
 		if resultError != nil {
 			loxerror.PrintErrorObject(resultError)
-			if !stdinFromTerminal {
-				return 1
-			}
-		}
-
-		if !stdinFromTerminal {
-			break
+			return 1
 		}
 	}
 
