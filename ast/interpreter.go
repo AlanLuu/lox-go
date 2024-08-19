AlanLuu/lox-go

commit 74f1bff19b0d130e9fe420a3baa3d6af303b938e

Allow multiplying lists with bigints Also fix issue of multiplying a float with a string not returning NaN when the float is on the left

ast/interpreter.go
@@ -501,7 +501,7 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 	handleBigNumString := func(left *big.Int, right *LoxString) (any, error) {
 		switch expr.Operator.TokenType {
 		case token.STAR:
-			if bigint.IsNegative(left) || bigint.IsZero(left) {
+			if bigint.IsZeroOrLess(left) {
 				return EmptyLoxString(), nil
 			}
 			var builder strings.Builder
@@ -540,6 +540,23 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 			}
 			newList := list.NewList[any]()
 			for i := int64(0); i < left; i++ {
+				for _, element := range right.elements {
+					newList.Add(element)
+				}
+			}
+			return NewLoxList(newList), nil
+		}
+		return math.NaN(), nil
+	}
+	handleBigNumList := func(left *big.Int, right *LoxList) (any, error) {
+		switch expr.Operator.TokenType {
+		case token.STAR:
+			if bigint.IsZeroOrLess(left) || len(right.elements) == 0 {
+				return EmptyLoxList(), nil
+			}
+			newList := list.NewList[any]()
+			one := bigint.BoolMap[true]
+			for i := big.NewInt(0); i.Cmp(left) < 0; i.Add(i, one) {
 				for _, element := range right.elements {
 					newList.Add(element)
 				}
@@ -914,8 +931,6 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 			return handleTwoBigFloats(bigfloat.New(left), right)
 		case bool:
 			return handleTwoFloats(left, boolMap[right], false)
-		case *LoxString:
-			return handleNumString(float64(left), right)
 		case nil:
 			return handleTwoFloats(left, 0, false)
 		}
@@ -933,6 +948,8 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 			return handleTwoBigInts(left, bigint.BoolMap[right])
 		case *LoxString:
 			return handleBigNumString(left, right)
+		case *LoxList:
+			return handleBigNumList(left, right)
 		case nil:
 			return handleTwoBigInts(left, bigint.BoolMap[false])
 		}
@@ -1013,22 +1030,11 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 				}
 				return left.NewLoxString(strings.Repeat(left.str, int(right))), nil
 			}
-			repeatBigInt := func(left *LoxString, right *big.Int) (*LoxString, error) {
-				if bigint.IsNegative(right) || bigint.IsZero(right) {
-					return EmptyLoxString(), nil
-				}
-				var builder strings.Builder
-				one := bigint.BoolMap[true]
-				for i := big.NewInt(0); i.Cmp(right) < 0; i.Add(i, one) {
-					builder.WriteString(left.str)
-				}
-				return left.NewLoxString(builder.String()), nil
-			}
 			switch right := right.(type) {
 			case int64:
 				return repeat(left, right)
 			case *big.Int:
-				return repeatBigInt(left, right)
+				return handleBigNumString(right, left)
 			case bool:
 				return repeat(left, int64(boolMap[right]))
 			case nil:
@@ -1134,6 +1140,8 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 			switch right := right.(type) {
 			case int64:
 				return repeat(left, right)
+			case *big.Int:
+				return handleBigNumList(right, left)
 			case bool:
 				return repeat(left, int64(boolMap[right]))
 			case nil:

bignum/bigint/bigint.go
@@ -19,6 +19,10 @@ func IsZero(x *big.Int) bool {
 	return len(x.Bits()) == 0
 }
 
+func IsZeroOrLess(x *big.Int) bool {
+	return IsZero(x) || IsNegative(x)
+}
+
 func String(x *big.Int) string {
 	return x.String() + "n"
 }

commit 213eeaf8d911b9655416fbf8c4a971a4131fb9b1

Allow bigints and bigfloats in dicts and sets Also fix bug with adding a float and a string introduced in 74f1bff19b0d130e9fe420a3baa3d6af303b938e

ast/interpreter.go
@@ -952,6 +952,11 @@ func (i *Interpreter) visitBinaryExpr(expr Binary) (any, error) {
 			return handleTwoBigFloats(bigfloat.New(left), right)
 		case bool:
 			return handleTwoFloats(left, boolMap[right], false)
+		case *LoxString:
+			switch expr.Operator.TokenType {
+			case token.PLUS:
+				return handleNumString(left, right)
+			}
 		case nil:
 			return handleTwoFloats(left, 0, false)
 		}

ast/loxbignumkey.go
@@ -1,0 +1,44 @@
+package ast
+
+import "math/big"
+
+type LoxBigNumKey struct {
+	str        string
+	isFloat    bool
+	isFloatInt bool
+}
+
+func NewLoxBigIntKey(x *big.Int) LoxBigNumKey {
+	return LoxBigNumKey{
+		str:        x.String(),
+		isFloat:    false,
+		isFloatInt: false,
+	}
+}
+
+func NewLoxBigFloatKey(x *big.Float) LoxBigNumKey {
+	return LoxBigNumKey{
+		str:        x.String(),
+		isFloat:    true,
+		isFloatInt: x.IsInt(),
+	}
+}
+
+func (l LoxBigNumKey) getBigNum() any {
+	if l.isFloat {
+		bigFloat := &big.Float{}
+		bigFloat.SetString(l.str)
+		return bigFloat
+	} else {
+		bigInt := &big.Int{}
+		bigInt.SetString(l.str, 0)
+		return bigInt
+	}
+}
+
+func (l LoxBigNumKey) String() string {
+	if l.isFloat && l.isFloatInt {
+		return l.str + ".0n"
+	}
+	return l.str + "n"
+}

ast/loxdict.go
@@ -2,12 +2,16 @@ package ast
 
 import (
 	"fmt"
+	"math/big"
 	"reflect"
 
+	"github.com/AlanLuu/lox/bignum/bigfloat"
+	"github.com/AlanLuu/lox/bignum/bigint"
 	"github.com/AlanLuu/lox/interfaces"
 	"github.com/AlanLuu/lox/list"
 	"github.com/AlanLuu/lox/loxerror"
 	"github.com/AlanLuu/lox/token"
+	"github.com/AlanLuu/lox/util"
 )
 
 func CanBeDictKeyCheck(key any) (bool, string) {
@@ -19,7 +23,17 @@ func CanBeDictKeyCheck(key any) (bool, string) {
 }
 
 func UnknownDictKey(key any) string {
-	return fmt.Sprintf("Unknown key '%v'.", key)
+	formatStr := "Unknown key '%v'."
+	switch key := key.(type) {
+	case float64:
+		return fmt.Sprintf(formatStr, util.FormatFloatZero(key))
+	case *big.Int:
+		return fmt.Sprintf(formatStr, bigint.String(key))
+	case *big.Float:
+		return fmt.Sprintf(formatStr, bigfloat.String(key))
+	default:
+		return fmt.Sprintf(formatStr, key)
+	}
 }
 
 type LoxDict struct {
@@ -155,6 +169,10 @@ func (l *LoxDict) getValueByKey(key any) (any, bool) {
 	var value any
 	var ok bool
 	switch key := key.(type) {
+	case *big.Int:
+		value, ok = l.entries[NewLoxBigIntKey(key)]
+	case *big.Float:
+		value, ok = l.entries[NewLoxBigFloatKey(key)]
 	case *LoxString:
 		value, ok = l.entries[LoxStringStr{key.str, key.quote}]
 	case *LoxRange:
@@ -167,6 +185,10 @@ func (l *LoxDict) getValueByKey(key any) (any, bool) {
 
 func (l *LoxDict) setKeyValue(key any, value any) {
 	switch key := key.(type) {
+	case *big.Int:
+		l.entries[NewLoxBigIntKey(key)] = value
+	case *big.Float:
+		l.entries[NewLoxBigFloatKey(key)] = value
 	case *LoxString:
 		l.entries[LoxStringStr{key.str, key.quote}] = value
 	case *LoxRange:
@@ -179,6 +201,10 @@ func (l *LoxDict) setKeyValue(key any, value any) {
 func (l *LoxDict) removeKey(key any) any {
 	keyItem := key
 	switch key := key.(type) {
+	case *big.Int:
+		keyItem = NewLoxBigIntKey(key)
+	case *big.Float:
+		keyItem = NewLoxBigFloatKey(key)
 	case *LoxString:
 		keyItem = LoxStringStr{key.str, key.quote}
 	case *LoxRange:
@@ -197,6 +223,8 @@ func (l *LoxDict) Iterator() interfaces.Iterator {
 	for key, value := range l.entries {
 		pair := list.NewList[any]()
 		switch key := key.(type) {
+		case LoxBigNumKey:
+			pair.Add(key.getBigNum())
 		case LoxStringStr:
 			pair.Add(NewLoxString(key.str, key.quote))
 		case LoxRangeDictSetKey:
@@ -205,6 +233,8 @@ func (l *LoxDict) Iterator() interfaces.Iterator {
 			pair.Add(key)
 		}
 		switch value := value.(type) {
+		case LoxBigNumKey:
+			pair.Add(value.getBigNum())
 		case LoxStringStr:
 			pair.Add(NewLoxString(value.str, value.quote))
 		case LoxRangeDictSetKey:

ast/loxset.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"fmt"
+	"math/big"
 	"reflect"
 
 	"github.com/AlanLuu/lox/interfaces"
@@ -145,6 +146,10 @@ func (l *LoxSet) Get(name *token.Token) (any, error) {
 func (l *LoxSet) add(element any) (bool, string) {
 	var theElement any
 	switch element := element.(type) {
+	case *big.Int:
+		theElement = NewLoxBigIntKey(element)
+	case *big.Float:
+		theElement = NewLoxBigFloatKey(element)
 	case *LoxString:
 		theElement = LoxStringStr{element.str, element.quote}
 	case *LoxRange:
@@ -166,6 +171,10 @@ func (l *LoxSet) add(element any) (bool, string) {
 func (l *LoxSet) contains(element any) bool {
 	var theElement any
 	switch element := element.(type) {
+	case *big.Int:
+		theElement = NewLoxBigIntKey(element)
+	case *big.Float:
+		theElement = NewLoxBigFloatKey(element)
 	case *LoxString:
 		theElement = LoxStringStr{element.str, element.quote}
 	case *LoxRange:
@@ -238,6 +247,10 @@ func (l *LoxSet) isProperSuperset(other *LoxSet) bool {
 func (l *LoxSet) remove(element any) bool {
 	var theElement any
 	switch element := element.(type) {
+	case *big.Int:
+		theElement = NewLoxBigIntKey(element)
+	case *big.Float:
+		theElement = NewLoxBigFloatKey(element)
 	case *LoxString:
 		theElement = LoxStringStr{element.str, element.quote}
 	case *LoxRange:
@@ -282,6 +295,8 @@ func (l *LoxSet) Iterator() interfaces.Iterator {
 	elements := list.NewList[any]()
 	for element := range l.elements {
 		switch element := element.(type) {
+		case LoxBigNumKey:
+			elements.Add(element.getBigNum())
 		case LoxStringStr:
 			elements.Add(NewLoxString(element.str, element.quote))
 		case LoxRangeDictSetKey:

util/util.go
@@ -47,6 +47,13 @@ func FormatFloat(f float64) string {
 	return strconv.FormatFloat(f, 'f', -1, 64)
 }
 
+func FormatFloatZero(f float64) string {
+	if FloatIsInt(f) {
+		return FormatFloat(f) + ".0"
+	}
+	return FormatFloat(f)
+}
+
 func IntOrFloat(f float64) any {
 	if FloatIsInt(f) {
 		return int64(f)