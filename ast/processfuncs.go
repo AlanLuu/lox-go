45c40ea6f8eacbd89d31623fee3d92057e9f202f
Alan Luu <alanluu4@gmail.com>
2024-09-25 12:06:17 -0700

    Implement runSetStd method on process class

--- ast/processfuncs.go
+++ ast/processfuncs.go
@@ -170,6 +170,22 @@ func (i *Interpreter) defineProcessFuncs() {
 		}
 		return NewLoxProcessResult(process.process.ProcessState), nil
 	})
+	processFunc("runSetStd", -1, func(in *Interpreter, args list.List[any]) (any, error) {
+		cmd, err := getExecCmd(methodName("runSetStd"), false, in, args)
+		if err != nil {
+			return nil, err
+		}
+		setStd(cmd)
+		process := NewLoxProcess(cmd)
+		if err := process.run(); err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				return NewLoxProcessResult(exitErr.ProcessState), nil
+			} else {
+				return nil, loxerror.RuntimeError(in.callToken, err.Error())
+			}
+		}
+		return NewLoxProcessResult(process.process.ProcessState), nil
+	})
 
 	i.globals.Define(className, processClass)
 }
