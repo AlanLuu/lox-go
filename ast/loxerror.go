2a96a86da3aa96453a17788f008fd12187380d88
Alan Luu <alanluu4@gmail.com>
2025-10-03 22:01:43 -0700

    Support nil error objects

--- ast/loxerror.go
+++ ast/loxerror.go
@@ -2,6 +2,8 @@ package ast
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
 
 	"github.com/AlanLuu/lox/loxerror"
 	"github.com/AlanLuu/lox/token"
@@ -19,6 +21,10 @@ func NewLoxError(theError error) *LoxError {
 	}
 }
 
+func (l *LoxError) isNil() bool {
+	return l.theError == nil
+}
+
 func (l *LoxError) Get(name *token.Token) (any, error) {
 	propertyName := name.Lexeme
 	if property, ok := l.properties[propertyName]; ok {
@@ -31,13 +37,65 @@ func (l *LoxError) Get(name *token.Token) (any, error) {
 		return property, nil
 	}
 	switch propertyName {
+	case "isNil":
+		return errorProperty(l.isNil())
 	case "message":
+		if l.isNil() {
+			return errorProperty(EmptyLoxString())
+		}
 		return errorProperty(NewLoxString(l.theError.Error(), '\''))
+	case "messageOnly":
+		if l.isNil() {
+			return errorProperty(EmptyLoxString())
+		}
+		errStr := l.theError.Error()
+		errStrLower := strings.ToLower(errStr)
+		index := strings.LastIndex(errStrLower, "\n[line ")
+		if index > 0 {
+			index--
+			inLoop := false
+			for index > 0 && unicode.IsSpace(rune(errStrLower[index])) {
+				inLoop = true
+				index--
+			}
+			if !inLoop {
+				index++
+			}
+			return errorProperty(NewLoxString(errStr[:index], '\''))
+		}
+		index2 := strings.LastIndex(errStrLower, "[line ")
+		if index2 == 0 {
+			index3 := strings.LastIndexByte(errStrLower, ']')
+			if index3 > 0 {
+				index3++
+				if index3 < len(errStrLower) {
+					for index3 < len(errStrLower) && unicode.IsSpace(rune(errStrLower[index3])) {
+						index3++
+					}
+					return errorProperty(NewLoxString(errStr[:index3], '\''))
+				}
+			}
+		} else if index2 > 0 {
+			index2--
+			inLoop := false
+			for index2 > 0 && unicode.IsSpace(rune(errStrLower[index2])) {
+				inLoop = true
+				index2--
+			}
+			if !inLoop {
+				index2++
+			}
+			return errorProperty(NewLoxString(errStr[:index2], '\''))
+		}
+		return errorProperty(NewLoxString(errStr, '\''))
 	}
 	return nil, loxerror.RuntimeError(name, "Error objects have no property called '"+propertyName+"'.")
 }
 
 func (l *LoxError) String() string {
+	if l.isNil() {
+		return fmt.Sprintf("<nil error object at %p>", l)
+	}
 	return fmt.Sprintf("<error object at %p>", l)
 }
 
