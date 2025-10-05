7b51f5790267d323f2be2392af324845287d7e7f
Alan Luu <alanluu4@gmail.com>
2025-10-05 11:08:52 -0700

    Improve error object code

--- ast/loxerror.go
+++ ast/loxerror.go
@@ -2,8 +2,6 @@ package ast
 
 import (
 	"fmt"
-	"strings"
-	"unicode"
 
 	"github.com/AlanLuu/lox/loxerror"
 	"github.com/AlanLuu/lox/token"
@@ -39,55 +37,42 @@ func (l *LoxError) Get(name *token.Token) (any, error) {
 	switch propertyName {
 	case "isNil":
 		return errorProperty(l.isNil())
+	case "lineNo":
+		const defLineNo = -1
+		if l.isNil() {
+			return int64(defLineNo), nil
+		}
+		switch err := l.theError.(type) {
+		case *loxerror.SyntaxErr:
+			return int64(err.Line), nil
+		case *loxerror.RuntimeErr:
+			if err.TheToken == nil {
+				return int64(defLineNo), nil
+			}
+			return int64(err.TheToken.Line), nil
+		default:
+			return int64(defLineNo), nil
+		}
 	case "message":
 		if l.isNil() {
 			return errorProperty(EmptyLoxString())
 		}
-		return errorProperty(NewLoxString(l.theError.Error(), '\''))
+		return errorProperty(NewLoxStringQuote(l.theError.Error()))
 	case "messageOnly":
 		if l.isNil() {
 			return errorProperty(EmptyLoxString())
 		}
-		errStr := l.theError.Error()
-		errStrLower := strings.ToLower(errStr)
-		index := strings.LastIndex(errStrLower, "\n[line ")
-		if index > 0 {
-			index--
-			inLoop := false
-			for index > 0 && unicode.IsSpace(rune(errStrLower[index])) {
-				inLoop = true
-				index--
+		switch err := l.theError.(type) {
+		case *loxerror.SyntaxErr:
+			return errorProperty(NewLoxStringQuote(err.Message))
+		case *loxerror.RuntimeErr:
+			return errorProperty(NewLoxStringQuote(err.Message))
+		default:
+			if element, ok := l.properties["message"]; ok {
+				return element, nil
 			}
-			if !inLoop {
-				index++
-			}
-			return errorProperty(NewLoxString(errStr[:index], '\''))
+			return errorProperty(NewLoxStringQuote(err.Error()))
 		}
-		index2 := strings.LastIndex(errStrLower, "[line ")
-		if index2 == 0 {
-			index3 := strings.LastIndexByte(errStrLower, ']')
-			if index3 > 0 {
-				index3++
-				if index3 < len(errStrLower) {
-					for index3 < len(errStrLower) && unicode.IsSpace(rune(errStrLower[index3])) {
-						index3++
-					}
-					return errorProperty(NewLoxString(errStr[:index3], '\''))
-				}
-			}
-		} else if index2 > 0 {
-			index2--
-			inLoop := false
-			for index2 > 0 && unicode.IsSpace(rune(errStrLower[index2])) {
-				inLoop = true
-				index2--
-			}
-			if !inLoop {
-				index2++
-			}
-			return errorProperty(NewLoxString(errStr[:index2], '\''))
-		}
-		return errorProperty(NewLoxString(errStr, '\''))
 	}
 	return nil, loxerror.RuntimeError(name, "Error objects have no property called '"+propertyName+"'.")
 }
--- loxerror/loxerror.go
+++ loxerror/loxerror.go
@@ -8,18 +8,48 @@ import (
 	"github.com/AlanLuu/lox/token"
 )
 
+type SyntaxErr struct {
+	Line    int
+	Where   string
+	Message string
+	FullMsg string
+}
+
+func (l *SyntaxErr) Error() string {
+	return l.FullMsg
+}
+
+type RuntimeErr struct {
+	TheToken *token.Token
+	Message  string
+	FullMsg  string
+}
+
+func (l *RuntimeErr) Error() string {
+	return l.FullMsg
+}
+
 func Error(message string) error {
 	return errors.New(message)
 }
 
 func GiveError(line int, where string, message string) error {
 	errorMsg := fmt.Sprintf("[line %v] Error%v: %v", line, where, message)
-	return errors.New(errorMsg)
+	return &SyntaxErr{
+		Line:    line,
+		Where:   where,
+		Message: message,
+		FullMsg: errorMsg,
+	}
 }
 
 func RuntimeError(theToken *token.Token, message string) error {
 	errorStr := message + "\n[line " + fmt.Sprint(theToken.Line) + "]"
-	return errors.New(errorStr)
+	return &RuntimeErr{
+		TheToken: theToken,
+		Message:  message,
+		FullMsg:  errorStr,
+	}
 }
 
 func PrintErrorObject(e error) {
